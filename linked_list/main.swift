08a54115973bbdb0f1157d722c625c456f2a4b0e
joe <jomon@accubits.com>
2021-11-23 13:55:12 +0530

    completed get and set method for linked list class

--- linked_list/main.swift
+++ linked_list/main.swift
@@ -105,35 +105,64 @@ public class LinkedList<T> {
     
     // o(1) -> removing a node from the first position
     func shift() {
-        if self.head == nil {
-            return
-        } else {
+        if self.head != nil  {
+            // if linked list contains two or more nodes
             let temp = self.head
             self.head = self.head?.next
             temp?.next = nil
             self.size -= 1
             
-            /* if linked list contains one item after the above
+            /* if linked list contains one node, after the above
             code the size will be empty so we re-adjust the
             tail to nil and because head is already nil */
             if self.isEmpty(){
                 self.tail = nil
             }
         }
+        // if head is nil return
+        return
     }
-
+    
+    // o(n) -> traversing through the list to get the value
+    func get(_ index: Int32) -> Node<T>?  {
+        // if index is less than zero and greater than length return nil
+        if index >= 0 || index < self.size {
+            var temp = self.head
+            for _ in 0..<index {
+                temp = temp?.next
+            }
+            return temp
+        }
+        return nil
+    }
+    
+    func set(_ index: Int32, _ value: T) -> Bool {
+        if let safeTempVal = self.get(index){
+            safeTempVal.data = value
+            return true
+        }
+        return false
+    }
+    
+    
+    
 }
 
 
 let dogList = LinkedList<String>()
 
 dogList.push("lab")
-//dogList.unShift("german sheperd")
-//dogList.unShift("bully")
-//dogList.push("husky")
+dogList.push("bulldog")
+dogList.push("husky")
+dogList.unShift("german sheperd")
 dogList.shift()
+let isSet: Bool = dogList.set(010, "beagle")
 
-dogList.shift()
+//if let safeVal = dogList.get(0){
+//    print(safeVal.data)
+//}
+
+
 
 print(dogList.toString())
 
